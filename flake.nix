a9e0b88b29364cd4c171e3e9b7c99e1d57515aa7
Brad <crustydog9@gmail.com>
2026-04-22 17:49:51 +1000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "C Nix dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = with pkgs; [
+          gcc
+        ];
+      };
+    };
+}
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Greetings, friend\n");
+    return 0;
+}
