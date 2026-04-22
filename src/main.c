dc7fa146802e9e27ebc414517f28a73a67e0c9b8
Brad <crustydog9@gmail.com>
2026-04-22 20:20:27 +1000

    Basic stuff

--- src/main.c
+++ src/main.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 
+
+int init_window () {
+    // Attempt to open default system terminal
+    // Return 1 if it fails
+    if (system("$TERM") != 0) {
+        printf("Couldn't open a terminal\n");
+        return 1;
+    }
+
+    return 0;
+}
+
 int main() {
-    initscr();
+    /*
+    initscr(); // Initialise curses mode
+
     printw("Greetings, friend.");
     refresh();
     getch();
-    endwin();
+
+
+    endwin(); // End curses mode - free memory, data, etc    
+    */
+
+    init_window();
 
     return 0;
 }
 
 
 
-/* NOTES
- *
- * Need ncurses
- * Open term in middle of screen windowed
- * Linux-BSD cross platform
- * Split into multiple files later
- * Init-sys agnostic
- * Find out init with function then use correct command
+/* TODO:
+ * 1. Open terminal when run
+ *      - Just use $TERM to begin with, make more robust later
+ *      - Ideally have the terminal appear as a floating window if possible
+ * 2. Print 'power off', 'reboot' and 'suspend' options in the middle of the terminal
+ * 3. Allow user to select an option
+ * 4. Handle selected option appropriately
+ *      - Need to determine init system
+ *      - Need to figure out how this works on BSD too
 */
