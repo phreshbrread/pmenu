0c9e8c3410c0ca245e57d385e83830a556881860
Brad <crustydog9@gmail.com>
2026-04-24 22:20:44 +1000

    Change generated binary name

--- build-run.sh
+++ build-run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-gcc --std=c99 -lmenu -lncurses -ltinfo ./src/main.c -o ./output/a.out && ./output/a.out
+gcc --std=c99 -lmenu -lncurses -ltinfo ./src/main.c -o ./output/pmenu && ./output/pmenu
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
+#include <menu.h>
 
 /* Declare variables */
 int startx  = 0;
@@ -7,41 +9,51 @@ int starty  = 0;
 int rows    = 0;
 int cols    = 0;
 int input   = 0;
+int i       = 0;
 
 char *options[] = {"Shutdown", "Reboot", "Suspend", "Cancel"};
-int option_count = sizeof(options) / sizeof(char *);    // Get number of options
+int option_count = sizeof(options) / sizeof(char *);    // Determine number of array entries
 /* -------------------------------------------------------------------------- */
 
 int main() {
+    /* Declare menu variables */
+    MENU *power_menu;
+    ITEM **items;
+    /* ------- */
+
     initscr();              // Initialise curses mode
     cbreak();               // Disable line buffering
     noecho();               // Don't echo characters back to terminal
     keypad(stdscr, TRUE);   // Enable special keys - need for arrows
+
+    items = (ITEM **)calloc(option_count + 1, sizeof(ITEM *));
+
+    /* Create menu items based on options array */
+    for (i; i < option_count; ++i) {
+        items[i] = new_item(options[i], options[i]);    // Add items here
+    }
+    items[option_count] = (ITEM *)NULL;                 // Terminate option list with null pointer
+    /* ---------------------------------------- */
+
+    power_menu = new_menu(items);     // Create power menu
+    post_menu(power_menu);            // Display power menu
     
-    /* Testing */
-    printw("Option count: %i", option_count);
-    /* ----------------------------------- */
+    refresh();                                  
 
-    // Loop to account for window resizing
+    /* Handle input here */
     while (true) {
-        getmaxyx(stdscr, rows, cols);   // Get size of terminal
-        erase();                        // Clear previous output
-
-        printw("Working");
-        refresh();
-
-       /* Handle input here */
-       input = getch();
-       if (input == KEY_UP) {
-           // Move selection up
-       } else if (input == KEY_DOWN) {
-           // Move selection down
-       } else {
-           // Do nothing
-       }
-       /* ----------- */
+    input = getch();
+    if (input == KEY_UP) {
+        menu_driver(power_menu, REQ_UP_ITEM);
+        break;
+    } else if (input == KEY_DOWN) {
+        menu_driver(power_menu, REQ_DOWN_ITEM);
+        break;
+    }
     }
 
+    /* ----------- */
+    
     endwin();   // End curses mode
     return 0;
 }
