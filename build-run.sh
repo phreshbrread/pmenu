4e4c5dcf493ff5b4fa88027d2104edecca449f6a
Brad <crustydog9@gmail.com>
2026-04-26 21:44:41 +1000

    Compiler flags

--- build-run.sh
+++ build-run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-gcc -g --std=c99 -lmenu -lncurses -ltinfo ./src/main.c -o ./output/pmenu && ./output/pmenu
+gcc -Wall -Wextra -Wpedantic -g --std=c99 -lmenu -lncurses -ltinfo ./src/main.c -o ./output/pmenu && ./output/pmenu
