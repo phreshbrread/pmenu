24a4e85263e50ec9291ebe994742fd6283ee89d1
Brad <crustydog9@gmail.com>
2026-04-28 13:50:18 +1000

    Switch to nix for building

--- build-run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-gcc -Wall -Wextra -Wpedantic -g --std=c99 -lmenu -lncurses -ltinfo ./src/main.c -o ./output/pmenu && ./output/pmenu
---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Example flake with a devShell";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gcc
+          ncurses
+          gdb
+        ];
+      };
+
+        packages.${system} = {
+          pmenu = pkgs.stdenv.mkDerivation {
+            pname = "pmenu";
+            version = "0.1.0";
+            src = ./.;
+
+            # Needed during build phase
+            nativeBuildInputs = with pkgs; [];
+
+            # Needed during runtime
+            buildInputs = with pkgs; [
+              ncurses
+            ];
+
+            # Build package
+            buildPhase = ''
+              gcc -Wall -Wextra -Wpedantic -g --std=c99 -lmenu -lncurses -ltinfo ./src/main.c -o pmenu
+            '';
+
+            # Install package
+            installPhase = ''
+              mkdir -p $out/bin
+              cp pmenu $out/bin/
+            '';
+
+            # Package metadata
+            meta = with pkgs.lib; {
+              description = "TUI power menu";
+              homepage = "https://github.com/phreshbrread/pmenu";
+              platforms = platforms.linux;
+            };
+          };
+
+          # Make default package when running 'nix build'
+          default = self.packages.${system}.pmenu;
+        };
+    };
+}
+
