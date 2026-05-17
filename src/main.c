902449e5d200938b9851f72d4fb7fd91b345f20f
PhreshBrread <crustydog9@gmail.com>
2026-05-17 21:08:22 +1000

    Edit help message to reflect changes

--- src/main.c
+++ src/main.c
@@ -79,12 +79,12 @@ void cancel_and_exit() {
 void show_help_message() {
     printf("Valid arguments:\n"
             "     --help\t\tShow this help message.\n"
-            "  -t --testing\t\tEnable testing mode (disables actual menu functions).\n"
+            "  -d --noconfirm\tDisable confirmation window.\n"
             "  -s --num-select\tEnable number key usage for menu options.\n"
+            "  -t --testing\t\tEnable testing mode (disables actual menu functions).\n"
             "  -v --version\t\tShow current version.\n"
             "\nNot yet implemented (planned):\n"
             "  -h --horizontal\tSet menu to display horizontally rather than vertically.\n"
-            "  -d --noconfirm\tDisable confirmation window.\n"
             "  -n --show-nums\tDisplay numbers before menu entries\n");
 }
 
